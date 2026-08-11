c616c77aafdeb09df73a3fc10c1fdc04b24107c4
Boss tendo <papobwattana@gmail.com>
2026-08-11 17:13:50 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import "@/styles/glass.css";
 import { Toaster } from "@/components/ui/toaster"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
