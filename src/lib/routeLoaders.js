2a6724ffd2a87e267f2dcf67f4629cc56216dc02
Boss tendo <papobwattana@gmail.com>
2026-08-13 10:48:38 +0000

    External agent changes

---
+++ src/lib/routeLoaders.js
@@ -0,0 +1,24 @@
+const routeLoaders = {
+  "/login": () => import("@/pages/Login"),
+  "/register": () => import("@/pages/Register"),
+  "/forgot-password": () => import("@/pages/ForgotPassword"),
+  "/reset-password": () => import("@/pages/ResetPassword"),
+  "/home": () => import("@/pages/Home"),
+  "/history": () => import("@/pages/History"),
+  "/history/announcements": () => import("@/pages/AnnouncementsHistory"),
+  "/history/news": () => import("@/pages/NewsHistory"),
+  "/feedback": () => import("@/pages/Feedback"),
+};
+
+export function loadRoute(pathname) {
+  const loader = routeLoaders[pathname];
+  return loader ? loader() : null;
+}
+
+export function preloadRoute(pathname) {
+  const loader = routeLoaders[pathname];
+  if (!loader) return;
+  void loader();
+}
+
+export const routeModules = routeLoaders;
