6bd33d48f0fd9d2eafa8459c625ee4113c053b23
Boss tendo <papobwattana@gmail.com>
2026-08-16 09:45:13 +0000

    External agent changes

--- dist-solid/assets/search-CKd5p4bK.js
+++
@@ -1,1 +0,0 @@
-import{I as r}from"./Icon-DkuC34Mq.js";import{c as a,m as c}from"./index-C6nrHAP4.js";var o=[["path",{d:"m21 21-4.34-4.34",key:"14j7rj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}]],m=e=>a(r,c(e,{iconNode:o,name:"search"})),p=m;export{p as s};
