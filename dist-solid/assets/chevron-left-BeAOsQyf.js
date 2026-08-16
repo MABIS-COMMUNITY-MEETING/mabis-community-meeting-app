644888a07cb89755045075ab323364d01049e5b8
Boss tendo <papobwattana@gmail.com>
2026-08-16 08:18:38 +0000

    External agent changes

---
+++ dist-solid/assets/chevron-left-BeAOsQyf.js
@@ -0,0 +1,1 @@
+import{I as o}from"./Icon-C92WDt1U.js";import{c as r,m as a}from"./index-BYrPjwg8.js";var t=[["path",{d:"m15 18-6-6 6-6",key:"1wnfg3"}]],m=e=>r(o,a(e,{iconNode:t,name:"chevron-left"})),c=m;export{c};
