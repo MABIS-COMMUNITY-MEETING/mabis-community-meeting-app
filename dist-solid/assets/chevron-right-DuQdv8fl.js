522f24d132124b126f50f3e8db60ae2b2ec49e24
Boss tendo <papobwattana@gmail.com>
2026-08-16 09:46:18 +0000

    External agent changes

--- dist-solid/assets/chevron-right-DuQdv8fl.js
+++
@@ -1,1 +0,0 @@
-import{I as e}from"./Icon-B1cyTy8C.js";import{c as o,m as a}from"./index-CwcaBDkB.js";var t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],m=r=>o(e,a(r,{iconNode:t,name:"chevron-right"})),c=m;export{c};
